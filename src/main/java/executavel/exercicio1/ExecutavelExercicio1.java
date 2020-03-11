46183337a7b8aba462b638315f2077284b13e57f
joao.essy <joao.essy@FLE211D013.alunos.sc.senac.br>
2020-03-11 10:14:32 -0300

    fiz certin

--- src/main/java/executavel/exercicio1/ExecutavelExercicio1.java
+++ src/main/java/executavel/exercicio1/ExecutavelExercicio1.java
@@ -15,11 +15,11 @@ import model.vo.exercicio1.Telefone;
 public class ExecutavelExercicio1 {
 
 	public static void main(String[] argumentosLinhaDeComando) {
-
+		
 		// TODO criar e SALVAR telefones
 		ArrayList<Telefone> telefones = new ArrayList<Telefone>();
 
-		// ExercÃ­cio 2
+		// Exercício 2
 		Cliente cliente1 = obterClienteDaTela();
 
 		// - Salvar no banco (APENAS TESTES, AINDA VIOLANDO O MVC)
@@ -27,6 +27,13 @@ public class ExecutavelExercicio1 {
 		String mensagem = clienteBO.salvar(cliente1);
 
 		JOptionPane.showMessageDialog(null, mensagem);
+		// 2
+		// - todos clientes
+		ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+		
+		//
+		Telefone tell = obterTelefoneDaTela(); 
+		
 
 	}
 
@@ -37,7 +44,7 @@ public class ExecutavelExercicio1 {
 
 		EnderecoDAO endDAO = new EnderecoDAO();
 		ArrayList<Endereco> listaEnderecos = endDAO.consultarTodos();
-		
+
 		Object[] enderecos = listaEnderecos.toArray();
 		Endereco enderecoSelecionado = (Endereco) JOptionPane.showInputDialog(null, 
 				"Selecione um endereço", "Endereço", 
@@ -48,5 +55,52 @@ public class ExecutavelExercicio1 {
 				new ArrayList<Telefone>(), enderecoSelecionado);
 
 		return novoCliente;
+	}
+	private static Telefone obterTelefoneDaTela() {
+		int opcaoSelecionada = JOptionPane.showConfirmDialog(null,"Telefone possui dono?", "Verificação", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
+		String codigoPais = JOptionPane.showInputDialog("Informe o codigo do país");
+		String ddd = JOptionPane.showInputDialog("Informe o DDD");
+		String numero = JOptionPane.showInputDialog("Informe o Número");
+		int movel = JOptionPane.showConfirmDialog(null, "Telefone móvel?","Verificação", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
+		int ativo = JOptionPane.showConfirmDialog(null, "Telefone Ativo?","Verificação", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
+
+		Telefone novoTelefone = new Telefone();
+		novoTelefone.setCodigoPais(codigoPais);
+		novoTelefone.setDdd(ddd);
+		novoTelefone.setNumero(numero);
+		if(movel == 0) {
+		novoTelefone.setMovel(true);
+			
+		}else {
+		novoTelefone.setMovel(false);	
+		}
+		if(ativo == 0) {
+		novoTelefone.setAtivo(true);
+		}else {
+		novoTelefone.setAtivo(false);
+		}
+		
+		
+		
+		if (opcaoSelecionada == JOptionPane.YES_OPTION) {
+
+			ClienteDAO cliDAO = new ClienteDAO();
+			ArrayList<Cliente> listaClientes = cliDAO.consultarTodos();
+
+			Object[] clientes = listaClientes.toArray();
+			Cliente clienteSelecionado = (Cliente) JOptionPane.showInputDialog(null, 
+					"Selecione um cliente", "Cliente", 
+					JOptionPane.QUESTION_MESSAGE, 
+					null, clientes, null);
+			
+			novoTelefone.setDono(clienteSelecionado);
+
+			
+		}
+
+
+		return novoTelefone;
+
+
 	}
 }
